bf168417f554a4447ca9162bbc2ec934cceb4056
anclark686 <anclark686@gmail.com>
2023-06-17 13:39:08 -0400

    adding frontend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:5173'
+        origins 'localhost:5173', 'reyaly-recipes-6afe74ae7b4d.herokuapp.com'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
