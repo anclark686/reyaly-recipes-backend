bc17f15b2e95dc9174711a42cf51b154f460c6a6
anclark686 <anclark686@gmail.com>
2023-06-17 15:51:01 -0400

    adding amplify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:5173', 'reyaly-recipes-6afe74ae7b4d.herokuapp.com', 'reyaly-recipes-frontend.vercel.app', 'reyaly-recipes.netlify.app'
+        origins 'localhost:5173', 'reyaly-recipes-6afe74ae7b4d.herokuapp.com', 'reyaly-recipes-frontend.vercel.app', 'reyaly-recipes.netlify.app', 'main.d3h1w2pu1ejnii.amplifyapp.com'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
