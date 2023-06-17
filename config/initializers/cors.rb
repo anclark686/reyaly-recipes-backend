daf229f7ecf2b667a39b8e7ce982c07ba8b9f3a4
anclark686 <anclark686@gmail.com>
2023-06-17 14:53:09 -0400

    adding vercel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:5173', 'reyaly-recipes-6afe74ae7b4d.herokuapp.com'
+        origins 'localhost:5173', 'reyaly-recipes-6afe74ae7b4d.herokuapp.com', 'https://reyaly-recipes-frontend.vercel.app/'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
